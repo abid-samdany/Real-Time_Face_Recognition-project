a4fd200530d134a67aa07d850d7857186d0dfe3c
git_tor <abidsamdany@gmail.com>
2018-10-07 11:42:49 +0600

    2nd commit

--- testrecognition.m
+++ testrecognition.m
@@ -1,5 +1,4 @@
 input_dir = 'C:\Users\MyPC\Desktop\Test\atnt\s1\';
-
  
 filenames = dir(fullfile(input_dir, '*.pgm'));
 num_images = length(filenames);
@@ -8,8 +7,17 @@ images = [];
 for n = 1:num_images
     filename = fullfile(input_dir, filenames(n).name);
     img = imread(filename);
-    
-    images(:, n) = img(:);
+%     figure
+%     imshow(img);
+    images(:,n) = img(:);
 end
 
+mean_face = mean(images, 2);
+% shifted_im = images - repmat(mean_face, 1, num_images);
+shifted_images =[];
+
+for n=1:num_images
+    shifted_images(:,n)= images(:,n)-mean_face;
+
+end
 
