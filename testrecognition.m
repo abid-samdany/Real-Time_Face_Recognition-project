7d9d4c2144802f800733e1d9152bb77177a93835
git_tor <abidsamdany@gmail.com>
2018-10-24 14:40:38 +0600

    6th commit

--- testrecognition.m
+++ testrecognition.m
@@ -1,6 +1,7 @@
-% Input train image directory
+% Load train image directory
 input_dir = 'G:\Test\face_database\';
- 
+image_dims =[120, 104];
+
 filenames = dir(fullfile(input_dir, '*.pgm'));
 num_images = length(filenames);
 images = [];
@@ -10,6 +11,7 @@ for n = 1:num_images
     filename = fullfile(input_dir, filenames(n).name);
     img = imread(filename);
     img = im2double(img);
+    img = imresize(img,image_dims);
     images(:,n) = img(:);
 end
 
@@ -28,7 +30,7 @@ end
 image_diff_tr= image_diff';
 L = image_diff_tr * image_diff;
 % eigen vector and value computation using Principle Component Analysis 
-[eig_vec, eig_val] = pca(L);
+[eig_vec, score, eig_val] = pca(L);
 % Large dimension eigen vector
 evec_ui= image_diff *eig_vec;
 % we set the no. of eigenface=10
@@ -42,8 +44,9 @@ weights = evec_ui' * image_diff;
 
 % input image for test
 input_img= imread('5.pgm');
+input_img = imresize(input_img,image_dims);
 
-%input image difference and input image weight
+% input image difference and input image weight
 input_img = im2double(input_img);
 input_img_diff= input_img(:)- mean_face;
 input_image_weight= evec_ui' * input_img_diff;
@@ -58,7 +61,8 @@ end
 
 % display the result
 figure();
-imshow([input_img ,reshape(images(:,match_index), [112, 92])]);
+imshow([input_img ,reshape(images(:,match_index), image_dims)]);
+title(sprintf('matches %s, score %f', filenames(match_index).name, match_score));
 
 
 %%
