ae4d5d55c496408928cdde3c378a05aae5b654df
git_tor <abidsamdany@gmail.com>
2019-01-25 12:16:37 +0600

    demo..

--- test-att/testrecognition.m
+++ test-att/testrecognition.m
@@ -34,7 +34,7 @@ L = image_diff_tr * image_diff;
 % Large dimension eigen vector
 evec_ui= image_diff *eig_vec;
 % we set the no. of eigenface=10
-num_eigenfaces = 10;
+num_eigenfaces = 8;
 evec_ui = evec_ui(:, 1:num_eigenfaces);
 
 % weight/ the feature vector calculation
@@ -68,18 +68,19 @@ title(sprintf('matches %s, score %f', filenames(match_index).name, match_score))
 
 
 %%
- % display the eigenvectors
-% figure;
+%  % display the eigenvectors
+% 
+% figure
 % for n = 1:num_eigenfaces
 % subplot(2, ceil(num_eigenfaces/2), n);
 % eig_vect = reshape(evec_ui(:,n), image_dims);
 % imagesc(eig_vect);
 % colormap(gray); 
 % end
-
-%%
- % display the eigenvalues
+% 
+% %%
+%  % display the eigenvalues
 % normalised_evalues = eig_val / sum(eig_val);
 % figure, plot(cumsum(normalised_evalues));
 % xlabel('No. of eigenvectors'), ylabel('Variance accounted for');
-% xlim([1 15]), ylim([0 1]), grid on;
+% xlim([1 30]), ylim([0 1]), grid on;
